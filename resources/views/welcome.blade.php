3a536df0e52783064b1e01bc94c15465a23ed0a4
stivenmon <stipelson@gmail.com>
2016-01-31 09:54:27 -0500

    "acualizado la template" "agregado un include para tener la nav en un archivo aparte y sea mas trabajable"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,20 @@
 @extends('admin.template.main')
 
+@section('tittle', 'Inicio')
+
 @section('content')
 
 <div class="container">
+  <div class="starter-template">
+  <h1>Quia</h1>
+    <p class="lead">
+        Use this document as a way to quickly start any new project.<br>
+        All you get is this text and a mostly barebones HTML document.
+    </p>
+</div>
+</div>
 
-      <div class="starter-template">
-        <h1>Holi laravel 5</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
 
-    </div><!-- /.container -->
+<a class="btn btn-success">Hola</a>
 
 @endsection
